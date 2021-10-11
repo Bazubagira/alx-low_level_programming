7ccb623bc0b7ce8f329fe9a983d7bb5827a9d290
Bazubagira <bernabazubagira@gmail.com>
2021-10-11 16:51:42 -0700

    advanced tasks

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *free_listint_safe - a function that frees a list
+ *@h: a pointer listint_t structure
+ *Return: the size of the list that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+size_t counter = 0;
+listint_t *temp;
+
+temp = *h;
+while (temp)
+{
+temp = *h;
+temp = temp->next;
+free_list(temp);
+counter++;
+}
+*h = NULL;
+
+return (counter);
+}
+/**
+ *free_list - a function that frees a listint_t recursively
+ *@head: a pointer to the listint_t structure
+ *Return: Nothing
+ */
+void free_list(listint_t *head)
+{
+listint_t *temp;
+
+if (head)
+{
+temp = head;
+temp = temp->next;
+free(temp);
+free_list(temp);
+}
+free(head);
+}
