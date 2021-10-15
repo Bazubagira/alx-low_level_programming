a2671cf5ff54acb02270d76a8e6c91c1654807a1
Bazubagira <bernabazubagira@gmail.com>
2021-10-15 03:26:34 -0700

    converts a binary to a number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int base = 1, result = 0, len = 0;
+int i;
+unsigned int dec_val = 0;
 
-if (b == NULL)
+if (!b)
 return (0);
 
-while (b[len])
-len++;
-
-while (len)
+for (i = 0; b[i]; i++)
 {
-if (b[len - 1] != '0' && b[len - 1] != '1')
+if (b[i] < '0' || b[i] > '1')
 return (0);
 
-if (b[len - 1] == '1')
-result += base;
-base *= 2;
-len--;
+ dec_val = 2 * dec_val + (b[i] - '0');
 }
-return (result);
+return (dec_val);
 }
 
